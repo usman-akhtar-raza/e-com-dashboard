fc72dcf3b7d138a1875208e50dfcd36b79d8ad91
usman <akhtarusman716@gmail.com>
2025-03-31 00:59:59 +0500

    resolving build error

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import AffiliateStat from "./models/affiliateStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
 dotenv.config();
 
